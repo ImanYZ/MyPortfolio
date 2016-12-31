853dbd2d4395bb0e1c349d718b722869057292d3
Iman Yeckehzaare <oneweb@umich.edu>
2016-12-31 01:28:51 -0500

    The first push of my portfolio.

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ADMINS = (('Iman', 'oneweb@umich.edu'))
 
-ALLOWED_HOSTS = ['iman.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['iman.us-west-2.elasticbeanstalk.com', 'www.iman.one']
 
 
 # Application definition
